a05073d3b935617c74e79051aa49d6f40e4a4acc
Agnieszka Rzeszuto <aga.rz@tlen.pl>
2014-05-11 16:33:03 +0200

    pierwszy commit

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+function gra(){
+	document.getElementById("tekst").innerHTML = '<embed src="pdf/gra.pdf" width="100%" height="400"/>'
+};
+
+function autor(){
+	document.getElementById('tekst').innerHTML = '<p>Agnieszka Rzeszuto 206323, 3 rok Informatyki UG</p>'
+};
+
+function nowa(){
+	document.getElementById('tekst').innerHTML = '<p>Wybierz w ile osób chcesz zagrać:</p><br/><button type="button" class="button">4 osoby</button><button type="button" class="button">8 osób</button>'
+}
